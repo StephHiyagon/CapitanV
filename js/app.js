2a935bd630b478c78928c333f862ecbef2c80fa5
stephHiyagon <mv.hiyagon@gmail.com>
2017-03-06 18:24:11 -0500

    buscando validar

--- js/app.js
+++ js/app.js
@@ -20,8 +20,10 @@ function mostrarS1(event){
 
 s1.addEventListener("click", mostrarS1);*/
 //var preg1= document.
+var boton1=document.getElementById('boton1');
+boton1.addEventListener('click',validar);
 
-window.addEventListener('load',function(){
+window.addEventListener('load',function(event){
 
   var nombre = prompt("Ingrese su nombre");
   console.log(nombre);
@@ -55,33 +57,39 @@ window.onclick = function(event) {
 	}
 }
 
-function validar(){
-var opciones = document.getElementsByName("name");
-console.log(opciones[0]);
+function validar(event){
+event.preventDefault();
+var opciones1 = document.getElementsByName("pregunta1");
+var opciones2 = document.getElementsByName("pregunta2");
+var opciones3 = document.getElementsByName("pregunta3");
+console.log(opciones1[0].value);
 var seleccionado = false;
-for(var i=0; i<opciones.length; i++) {
-  if(opciones[i].checked) {
+for(var i=0; i<opciones1.length; i++) {
+  if(opciones1[i].checked) {
     seleccionado = true;
     break;
+  } else {
+    alert("Debe seleccionar alguna respuesta de la primera pregunta");
   }
 }
 
-if(!seleccionado) {
+/*if(!seleccionado) {
   return false;
-}
+}*/
 }
 
 /*    }
 
-    return revisar1();
+    return revisarQ1();
   }*/
 
 
-/*
-function revisar1 (){
-  var contador=0;
-  var preg1 = document.getElementById('p1');
-  var preg2 = document.getElementById('p2');
-  var preg3 = document.getElementById('p3');
 
-}*/
+function revisarQ1 (){
+  var contador=0;
+//  si selecciono el radioboton 1 en la primera pregunta es correcto y suma 1 en el contador sino 0
+//  si selecciono el radioboton 2 en la segunda pregunta es correcto...
+// si selecciono el radioboton 3 en la tercera pregunta es correcto...Al final retorna un texto con el numero de rptas correctas y desaparece el quiz
+
+
+}
